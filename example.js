903def67ae1249e001f5484ab3d596e5537773b0
Ionică Bizău <bizauionica@gmail.com>
2016-06-12 10:37:31 +0300

    Removed trailing slash

--- example.js
+++ example.js
@@ -1,4 +1,4 @@
-var getMeta = require("./")
+var getMeta = require('.')
 
 // Pass and HTML string
 getMeta("<meta name='page' content='index'><meta name='description' content='This is the index page'>")
